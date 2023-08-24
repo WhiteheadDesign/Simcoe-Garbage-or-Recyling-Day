6de8939667178db986694e85ef0f8b358163f2aa
Johnathan Whitehead <jwhiteheadstayner@gmail.com>
2023-08-24 10:05:37 -0400

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -1,6 +1,8 @@
 #include <WiFiUdp.h>
 #include <NTPClient.h>
 #include <ESP8266WiFi.h>
+#define ssid  "MAH-SHWAMP"
+#define password  "nintendo"
 const int Garbage = D5; //Green 1st and 3rd week of the month Odd
 const int Recycling = D6; //Blue 2nd and 4th week of the month even
 
@@ -11,7 +13,7 @@ void setup() {
   pinMode(Garbage, OUTPUT);
   pinMode(Recycling, OUTPUT);
   Serial.begin(115200);
-  WiFi.begin("MAH-SHWAMP_BACKUP", "nintendo");
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
